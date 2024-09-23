cc085614cbcdd5b5074d6f2c03dcd7d5cf4fcbe3
Sudhanshu-code <mail.sudhanshug@gmail.com>
2024-09-23 14:58:40 +0530

    Create utillity function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+    });
+    app.listen(process.env.POST, () => {
+      console.log("Mongo DB Connection successfull");
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+//can use any one of the bellow
+//using then catch promise
+const asynHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asynHandler };
+
+//using async await
+
+// const asynHandlerr = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
